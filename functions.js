6845ef72f2380694ccf473f1d04c5484c30bdf1d
Cindy Leiva <cindyleiva376@gmail.com>
2023-06-07 11:01:54 +0200

    funcionalidad button-favourite

---
+++ functions.js
@@ -0,0 +1,20 @@
+// Container of data
+const containerOfData = document.querySelector('.curious-facts')
+const containerOfFavourites = document.querySelector('.facts-favourites')
+
+// Buttons
+const favouriteButton = document.querySelector('.button-favourite')
+const explorerButton = document.querySelector('.button-exploration')
+
+explorerButton.addEventListener('click', function () {
+    fetch('https://uselessfacts.jsph.pl/api/v2/facts/random')
+    .then(response => response.json())
+    .then(json => {
+        containerOfData.textContent = json.text;
+        return json;
+    })
+    .catch(error => {
+        console.log('Ha ocurrido un error:', error);
+    });
+});
+
