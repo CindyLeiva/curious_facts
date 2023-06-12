08f24b330f782f81d0183dd5c4163d38a0e95e15
JacinemiUA <131624010+JacinemiUA@users.noreply.github.com>
2023-06-12 15:02:36 +0200

    Eliminación de comentarios en HTML, rarandomFact/fact.test.js, randomFact/favouriteFact.js, randomFact/favouritesFact.test.js

--- randomFact/fact.test.js
+++ randomFact/fact.test.js
@@ -1,6 +1,4 @@
-//const fetch = require('node-fetch');
-
-const { getRandomFact, deleteFact } = require('./getRandomFact.js');
+const {deleteFact} = require('./getRandomFact.js');
 
 describe('test deleteFact function', () => {
     it('deleteFact is a function', () => {
@@ -8,13 +6,10 @@ describe('test deleteFact function', () => {
     });
 
     it('remove a textFact', () => {
-        // Simulamos el elemento h4.curious-fact
         document.body.innerHTML = '<h4 class="curious-fact">Facto de ejemplo</h4>';
 
-        // Llamamos a la función deleteFact
         deleteFact();
 
-        // Verificamos que el elemento haya sido removido
         const actualFact = document.querySelector('h4.curious-fact');
         expect(actualFact).toBeNull();
     });
@@ -27,24 +22,3 @@ describe('test deleteFact function', () => {
     });
 });
 
-// describe('test getRandomFact function', () => {
-//     it('test', async () => {
-//         // Simula la respuesta de la API
-//         const mockResponse = { text: 'Example fact' };
-//         jest.spyOn(global, 'fetch').mockResolvedValueOnce({
-//             json: jest.fn().mockResolvedValueOnce(mockResponse),
-//         });
-
-//         // Configura el DOM con un contenedor vacío
-//         document.body.innerHTML = '<div id="curious-fact"></div>';
-
-//         // Llama a la función
-//         await getRandomFact();
-
-//         // Verifica que se haya agregado un elemento al contenedor
-//         const containerFact = document.getElementById('curious-fact');
-//         expect(containerFact.children.length).toBe(1);
-//         expect(containerFact.children[0].tagName).toBe('h4');
-//         expect(containerFact.children[0].textContent).toBe('Example fact');
-//     });
-// });
--- randomFact/favouriteFact.js
+++ randomFact/favouriteFact.js
@@ -1,13 +1,10 @@
 let factList = [];
 
 function selectFavourites () {
-    //Llamamos al contenedor del FACT
     const curiousFact = document.getElementById('curious-fact');
-    //Aqui asignamos el FACT a la variable FavFact
     const favFact = curiousFact.textContent;
     const newFactLi = document.createElement('li');
     newFactLi.innerHTML = favFact;
-    //Aqui llamamos al contenedor de los Fact Favorites
     const favouriteFact = document.querySelector('.facts-favourites ul');
     if( factList.includes(newFactLi.innerHTML)) {
         alert('This message already exist')
